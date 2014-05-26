9627c8d292303f1d9e50ea9276aef3602fae1adb
pdewan <dewan@cs.unc.edu>
2014-05-26 05:05:12 -0400

    Changed formatting

--- src/util/session/ServerMessageFilter.java
+++ src/util/session/ServerMessageFilter.java
@@ -2,7 +2,6 @@ package util.session;
 
 public interface ServerMessageFilter extends MessageFilter<SentMessage> {
 	public void userJoined(String userName);
-
 	public void userLeft(String userName);
 
 }
