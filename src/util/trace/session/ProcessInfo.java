ab7a9700cd04ffbd730f78a46391ef17312fcdda
pdewan <dewan@cs.unc.edu>
2014-05-31 04:42:43 -0400

    Made a variable protected and changed its name

--- src/util/trace/session/ProcessInfo.java
+++ src/util/trace/session/ProcessInfo.java
@@ -5,20 +5,22 @@ import java.util.Date;
 import util.trace.TraceableInfo;
 
 public class ProcessInfo extends TraceableInfo {
-	String processName;
+	protected String processName;
 	public ProcessInfo(String aMessage, 
 			String aProcessName,
 			Object aFinder) {
 		super(aMessage, aFinder);
 		processName = aProcessName;
 	}
-	public String getUserName() {
+	public String getProcessName() {
 		return processName;
 	}
 	public static String toTime(Date aDate) {
 		return aDate.getHours() + ":" + aDate.getMinutes() + ":" + aDate.getSeconds();
 	}
 	public static String toString (String aProcessName) {
+		if (aProcessName == null || (aProcessName.isEmpty()) )
+			return "";
 		long time = System.currentTimeMillis();
 		Date date = new Date(time);
 		
