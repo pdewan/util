e337f9c998a2d827374621c5e11aeea777bc585a
pdewan <dewan@cs.unc.edu>
2014-06-05 11:58:16 -0400

    More parsing

--- src/util/trace/session/AddressedMessageInfo.java
+++ src/util/trace/session/AddressedMessageInfo.java
@@ -4,25 +4,55 @@ import util.trace.TraceableInfo;
 
 public class AddressedMessageInfo extends MessageInfo {
 	
-	Object data;
+//	Object data;
 	String sourceOrDestination;
 	public AddressedMessageInfo(String aMessage, String aProcessName, Object aDataItem,
 			String aSourceOrDestination,  Object aFinder) {
 		super(aMessage, aProcessName,  aDataItem, aFinder);
-		this.data = aDataItem;
+//		this.data = aDataItem;
 		this.sourceOrDestination = aSourceOrDestination;
 	}
-	public Object getData() {
-		return data;
+//	public Object getData() {
+//		return data;
+//	}
+	public AddressedMessageInfo(String aMessage,  
+			String aSourceOrDestination,  MessageInfo aMessageInfo) {
+		super(aMessage, aMessageInfo);
+//		this.data = aDataItem;
+		this.sourceOrDestination = aSourceOrDestination;
 	}
+	
+	public AddressedMessageInfo(String aMessage,  
+			AddressedMessageInfo anAddressedMessageInfo) {
+		this(aMessage, anAddressedMessageInfo.getSourceOrDestination(), anAddressedMessageInfo);
+//		
+	}
+	
+	
 	public String getSourceOrDestination() {
 		return sourceOrDestination;
 	}
 	
+	public static final String ADDRESS = "Address";
+	
+	public static String getAddress(String aMessage) {
+		return getArgs(aMessage, ADDRESS).get(0);
+	}
+	
+	public static AddressedMessageInfo toTraceable(String aMessage) {
+		MessageInfo aMessageInfo = MessageInfo.toTraceable(aMessage);
+		return new AddressedMessageInfo(aMessage, getAddress(aMessage), aMessageInfo);
+	}
+	
+	
+	
 	public static String toString(String aProcessName, Object aDataItem,
 			String aSourceOrDestination) {
 //		return toString(aProcessName) + " " + ((aDataItem == null)?"": "Msg(" + aDataItem + ")") + " Peer(" + aSourceOrDestination + ")"  ;
-		return toString(aProcessName, aDataItem) + " " + "Address(" + aSourceOrDestination + ")"  ;
+		return toString(aProcessName, aDataItem) + " " + 
+		ADDRESS + "(" +
+//		"Address(" + 
+		aSourceOrDestination + ")"  ;
 
 	}
 	
--- src/util/trace/session/MessageInfo.java
+++ src/util/trace/session/MessageInfo.java
@@ -6,17 +6,45 @@ public class MessageInfo extends ProcessInfo {
 
 	Object data;
 
-	public MessageInfo(String aProcessName, String aMessage, Object aDataItem, Object aFinder) {
-		super(aProcessName, aMessage, aFinder);
+	public MessageInfo(String aMessage, String aProcessName, Object aDataItem, Object aFinder) {
+		super(aMessage, aProcessName, aFinder);
 		this.data = aDataItem;
-
 	}
+	
+	public MessageInfo(String aMessage, Object aDataItem, ProcessInfo aProcessInfo) {
+		super(aMessage, aProcessInfo);
+		this.data = aDataItem;
+	}
+	
+	public MessageInfo(String aMessage, MessageInfo aMessageInfo) {
+		this (aMessage, aMessageInfo.getData(), aMessageInfo);
+//		super(aMessage, aProcessInfo);
+//		this.data = aDataItem;
+	}
+	
+	
 	public Object getData() {
 		return data;
 	}
+	
+	public static final String MESSAGE = "Msg";
+	
+	public static String getData(String aMessage) {
+		return getArgs(aMessage, MESSAGE).get(0);
+	}
+	
+	public static MessageInfo toTraceable(String aMessage) {
+		ProcessInfo aProcessInfo = toTraceable(aMessage);
+		return new MessageInfo(aMessage, getData(aMessage), aProcessInfo);
+	}
 
 	public static String toString(String aProcessName, Object aDataItem) {
-		return toString(aProcessName) + (aDataItem  != null?  " Msg(" + aDataItem.toString() +")" :"") ;
+//		return toString(aProcessName) + (aDataItem  != null?  " Msg(" + aDataItem.toString() +")" :"") ;
+		return toString(aProcessName) + (aDataItem  != null?  
+//				" Msg(" +
+				" " + MESSAGE + "(" +
+				aDataItem.toString() +")" :"") ;
+
 	}
 	
 }
