cc7c3a69f6c67543f1b94f4f82417be22e88f001
pdewan <dewan@cs.unc.edu>
2014-06-14 11:54:02 -0400

    Added general Object search

---
+++ src/util/trace/query/ObjectSearch.java
@@ -0,0 +1,56 @@
+package util.trace.query;
+
+import util.trace.Traceable;
+import util.trace.TraceableInfo;
+import util.trace.console.ConsoleInput;
+
+public class ObjectSearch extends TraceableInfo {
+	Object previousObject;
+	Object laterObject;
+	Object expectedObject;
+	public static final String PREVIOUS = "previous";
+	public static final String LATER = "later";
+	public static final String EXPECTED = "expected";
+
+	public ObjectSearch(String aMessage, Object aPreviousObject, Object anExpectedObject, Object aLaterObject, Object aFinder) {
+		super(aMessage, aFinder);
+		previousObject = aPreviousObject;
+		laterObject = aLaterObject;
+	}
+	public ObjectSearch(String aMessage, Object aPreviousObject, Object anExpectedObject, Object aLaterObject) {
+		this(aMessage, aPreviousObject, anExpectedObject, aLaterObject, null);
+	}
+	public Object getPreviousObject() {
+		return previousObject;
+	}
+	public Object getLaterObject() {
+		return laterObject;
+	}
+	public String toString (Object aPreviousObject, Object anExpectedObject, Object aLaterObject) {
+		return 
+			aPreviousObject == null? "": PREVIOUS + Traceable.NESTED_LEFT_MARKER + (aPreviousObject) + Traceable.NESTED_RIGHT_MARKER
+			+ " "	
+			+ anExpectedObject == null? "": EXPECTED + Traceable.NESTED_LEFT_MARKER + (anExpectedObject) + Traceable.NESTED_RIGHT_MARKER
+			
+			+ aLaterObject == null? "": LATER + Traceable.NESTED_LEFT_MARKER + (aLaterObject) + Traceable.NESTED_RIGHT_MARKER;
+	}
+	
+	public static String getPrevious(String aMessage) {
+		return getNestedArgs(aMessage, PREVIOUS).get(0);
+	}
+	public static String getExpected(String aMessage) {
+		return getNestedArgs(aMessage, EXPECTED).get(0);
+	}
+	public static String getLater(String aMessage) {
+		return getNestedArgs(aMessage, LATER).get(0);
+	}
+	
+	public static Traceable toTraceable(String aMessage) {
+		return new ObjectSearch (aMessage, 
+				getPrevious(aMessage),
+				getExpected(aMessage),
+				getLater(aMessage));
+	}
+	
+
+}
