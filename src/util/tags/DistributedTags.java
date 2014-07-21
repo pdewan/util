017574e3a9fc26139e1a20c848a2fa5a87fb88d5
pdewan <dewan@cs.unc.edu>
2014-07-21 05:48:44 -0400

    Added registry and some other tags

--- src/util/tags/DistributedTags.java
+++ src/util/tags/DistributedTags.java
@@ -1,11 +1,16 @@
 package util.tags;
 
 public interface DistributedTags {
+	String REGISTRY = "Registry";
 	String SERVER = "Server";
 	String CLIENT = "Client";
 	String SESSION_MANAGER = "Session Manager";
 	String CAUSAL = "CAUSAL";
 	String OT = "Operation Transformer";
+	String RMI = "RMI";
+	String SOCKETS = "Sockets";
+	String NIO = "NIO";
+	String GIPC = "GIPC";
 	// some typical client names
 	String ALICE = "Alice";
 	String BOB = "Bob";
