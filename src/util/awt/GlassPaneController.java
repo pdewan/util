a2b779fe76b61f2b506df6569739d0e143f55657
pdewan <dewan@cs.unc.edu>
2014-08-07 08:02:03 -0400

    size

--- src/util/awt/GlassPaneController.java
+++ src/util/awt/GlassPaneController.java
@@ -1,6 +1,9 @@
 package util.awt;
 
 public interface GlassPaneController {
+	int getPointerSize();
+
+	void setPointerSize(int aSize);
 	int getPointerWidth();
 
 	void setPointerWidth(int aWidth);
