484308898523308356bd9102b90e6807d55c6028
pdewan <dewan@cs.unc.edu>
2014-09-27 18:14:23 -0400

    More tags

--- src/util/tags/FunctionTags.java
+++ src/util/tags/FunctionTags.java
@@ -1,10 +1,10 @@
 package util.tags;
 
-public class FunctionTags {
-	String CONCURRENCY_CONTROLLED = "concurrency controlled";	
-	String LOCK_CONTROLS = "lock controls";
-	String LOCK_REQUEST = "lock request";
-	String LOCK_RELEASE = "lock release";	
-	String ACCESS_CONTROLLED = "access controlled";
+public interface FunctionTags {
+	 String CONCURRENCY_CONTROLLED = "concurrency controlled";	
+	 String LOCK_CONTROLS = "lock controls";
+	 String LOCK_REQUEST = "lock request";
+	 String LOCK_RELEASE = "lock release";	
+	 String ACCESS_CONTROLLED = "access controlled";
 
 }
