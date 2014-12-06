4cf1f481f2460ae8afda78344a2109167ee69908
pdewan <dewan@cs.unc.edu>
2014-12-05 20:27:31 -0500

    changed parameter name

--- src/util/session/ServerSentMessageFilterSelector.java
+++ src/util/session/ServerSentMessageFilterSelector.java
@@ -1,14 +1,14 @@
 package util.session;
 
 public class ServerSentMessageFilterSelector {
-	static ServerMessageFilterCreator queuerFactory = new AServerMessageForwarderCreator();
+	static ServerMessageFilterCreator factory = new AServerMessageForwarderCreator();
 
 	public static ServerMessageFilterCreator getMessageQueuerFactory() {
-		return queuerFactory;
+		return factory;
 	}
 
 	public static void setMessageFilterFactory(
 			ServerMessageFilterCreator theFactory) {
-		queuerFactory = theFactory;
+		factory = theFactory;
 	}
 }
