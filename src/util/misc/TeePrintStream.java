de3fb56a5454814f2789e14397b80a418b23faf3
pdewan <dewan@cs.unc.edu>
2016-06-15 08:19:51 -0400

    TeePrintStream now has print (double) and print (int)

--- src/util/misc/TeePrintStream.java
+++ src/util/misc/TeePrintStream.java
@@ -19,6 +19,16 @@ public class TeePrintStream extends PrintStream{
 		super.println(newLine);
 		stdout.println(newLine);
 	}
+	@Override
+	public void println(double aDouble) {
+		super.println(aDouble);
+		stdout.println(aDouble);
+	}
+	@Override
+	public void println(int anInt) {
+		super.println(anInt);
+		stdout.println(anInt);
+	}
 	
 //	static {
 //		stdout = System.out;
